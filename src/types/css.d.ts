f4e081ecb56bf85a6739c00a332933374638603d
Jayam shanmukha shivanvitha <jayamshanmukha.cs24@bmsce.ac.in>
2025-11-18 22:04:57 +0530

    changes

---
+++ src/types/css.d.ts
@@ -0,0 +1,24 @@
+declare module '*.css';declare module '*.css' {
+
+declare module '*.css' {
+	const content: { [className: string]: string };
+	export default content;
+}
+
+declare module '*.module.css' {
+	const content: { [className: string]: string };
+	export default content;
+}
+
+declare module '*.scss' {
+	const content: { [className: string]: string };
+	export default content;
+}
+
+declare module '*.module.scss' {
+	const content: { [className: string]: string };
+	export default content;
+}
+
+}
+
